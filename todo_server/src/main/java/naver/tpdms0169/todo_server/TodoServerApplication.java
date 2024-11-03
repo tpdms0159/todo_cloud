ad26859fb777f3c91414a7bfd1361232ff152b53
tpdms0159 <tpdms0159@naver.com>
2024-11-03 18:08:18 +0900

    add : init server&client

---
+++ todo_server/src/main/java/naver/tpdms0169/todo_server/TodoServerApplication.java
@@ -0,0 +1,13 @@
+package naver.tpdms0169.todo_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoServerApplication.class, args);
+    }
+
+}
---
+++ todo_server/src/test/java/naver/tpdms0169/todo_server/TodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.tpdms0169.todo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
